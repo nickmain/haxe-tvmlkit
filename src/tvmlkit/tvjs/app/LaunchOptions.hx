333cae1438924d84eed9a206e4e1bcfc6f412613
nickmain <david.nick.main@gmail.com>
2016-08-25 20:57:21 -0700

    Dynamic reload data to allow member access

--- src/tvmlkit/tvjs/app/LaunchOptions.hx
+++ src/tvmlkit/tvjs/app/LaunchOptions.hx
@@ -6,7 +6,7 @@ abstract LaunchOptions(Dynamic) {
 
     @:arrayAccess
     public inline function get(key: String) { return Reflect.field(this, key); }
-    public inline function getReloadData(): Null<{}> { return this.reloadData; }
+    public inline function getReloadData(): Null<Dynamic> { return this.reloadData; }
     public inline function getLocation(): Null<String> { return this.location; }
     public inline function getLaunchContext(): Null<String> { return this.launchContext; }
 }
